67b0a65ff350a2ace42f252e8faf240718502c83
zz-1573 <160580433+zz-1573@users.noreply.github.com>
2024-02-20 16:00:59 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,14 @@ let myHeading = document.querySelector("h1");
 function setUserName() {
   let myName = prompt("Please enter your name。");
   localStorage.setItem("name", myName);
-  myHeading.textContent = "Jimmy is a Good Man，" + myName;
+  myHeading.textContent = "我的web第一页！，" + myName;
 }
 
 if (!localStorage.getItem("name")) {
   setUserName();
 } else {
   let storedName = localStorage.getItem("name");
-  myHeading.textContent = "Jimmy is a Bad Man，" + storedName;
+  myHeading.textContent = "Jimmy is a good Man，" + storedName;
 }
 
 myButton.onclick = function () {
