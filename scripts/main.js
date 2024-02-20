455719cbfb6d12efd050358193310bc01de73fcc
zz-1573 <160580433+zz-1573@users.noreply.github.com>
2024-02-20 15:48:38 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  let myName = prompt("Please enter your name。");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = "Jimmy is a Good Man，" + myName;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem("name");
+  myHeading.textContent = "Jimmy is a Bad Man，" + storedName;
+}
+
+myButton.onclick = function () {
+  setUserName();
+};
